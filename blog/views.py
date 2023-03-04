324adca0712e5dee1326521d925439681fcf6c71
yashasvini <ysmarina1234@gmail.com>
2023-03-04 22:14:56 +0530

    html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+"""
+As you can see, we created a function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html.
+"""
+
+"""
+A view is a place where we put the "logic" of our application. It will request information from the model you created before and pass it to a template. We'll create a template in the next chapter. Views are just Python functions that are a little bit more complicated than the ones we wrote in the Introduction to Python chapter.
+"""
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,7 @@
-"""mysite URL Configuration
+"""
+These triple strings are called docstrings
+
+mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +17,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
+
+"""
+- We also want to keep the mysite/urls.py file clean, so we will import URLs from our blog application to the main mysite/urls.py file.
+"""
