9ff853ad4d646096a0262e5f0ab7882736559402
yashasvini <ysmarina1234@gmail.com>
2023-03-05 12:01:00 +0530

    debugged

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': post})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 """
 As you can see, we created a function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html.
